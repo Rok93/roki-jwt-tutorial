6a624380742ff84329d978d451f9af5226c20c89
rok93 <goodboy302@naver.com>
2021-05-13 02:19:28 +0900

    Initial commit

---
+++ src/main/java/com/example/rokijwttutorial/RokiJwtTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.rokijwttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokiJwtTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RokiJwtTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rokijwttutorial/RokiJwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rokijwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RokiJwtTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
